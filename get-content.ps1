93abd960c46a2d1d564f9c41131e38ffd635540b
Rick Jury <rjury@sumologic.com>
2020-04-30 17:40:30 +1200

    add ContentExportJob

--- get-content.ps1
+++ get-content.ps1
@@ -1,6 +1,5 @@
 # powershell functions to export content, and put into terraform format (useful for prototyping content or migration)
 Param(
-        [parameter(Mandatory=$false)][string] $FolderNameToExport = "Search Audit Custom",
         [parameter(Mandatory=$false)][string] $Parent = "Personal",
         [parameter(Mandatory=$false)][string] $endpoint="https://api.us2.sumologic.com",
         [parameter(Mandatory=$false)][string] $accessid=$env:SUMO_ACCESS_ID_BE,
@@ -57,8 +56,63 @@ function get-ContentPath {
     Param(
         [parameter(Mandatory=$true)][string] $id 
 )
-    return invoke-sumo -path "content/$id/path"
+    return (invoke-sumo -path "content/$id/path").path
 }
+
+function start-ContentExportJob {
+    Param(
+        [parameter(Mandatory=$true)][string] $id 
+)
+    return @{'contentId' = $id; 'jobId' = (invoke-sumo -path "content/$id/export/" -method 'POST').id }
+}
+
+
+function get-ContentExportJobStatus {
+    Param(
+        [parameter(Mandatory=$true)][string] $job,
+        [parameter(Mandatory=$true)][string] $id
+
+)
+    return invoke-sumo -path "content/$id/export/$job/status" -method 'GET'
+}
+
+function get-ContentExportJobResult {
+    Param(
+        [parameter(Mandatory=$true)][string] $job,
+        [parameter(Mandatory=$true)][string] $id
+
+)
+    return invoke-sumo -path "content/$id/export/$job/result" -method 'GET'
+}
+
+function run-ContentExportJob {
+    Param(
+        [parameter(Mandatory=$true)][string] $id ,
+        [parameter(Mandatory=$false)][string] $poll_secs=1,
+        [parameter(Mandatory=$false)][string] $max_tries=10
+)
+    $job = start-ContentExportJob -id $id
+    $tries = 0
+
+    While  (($job) -and ($max_tries -gt $tries)) {
+        $tries = $tries +1     
+        Write-Verbose "polling id: $id $($job['jobId']). try: $tries of $max_tries"
+        $job_state = get-ContentExportJobStatus -job $job['jobId'] -id $id
+        Write-Verbose  ($job_state.status)
+        if ($job_state.status -eq 'Success') {
+            
+             break
+        } else {
+            Start-Sleep -Seconds $poll_secs
+        }
+    }   
+    Write-Verbose "job poll completed: status: $($job_state.status) contentId: $id jobId: $($job['jobId'])"
+    if ($job_state.status -eq 'Success') {
+        $result = get-ContentExportJobResult -job $job['jobId'] -id $id
+    } else { Write-Error 'Job failed or timed out';}
+    return  $result
+}
+
 
 if ($Parent -eq "Personal") {
     $parent_folder = get-PersonalFolder
@@ -67,6 +121,12 @@ if ($Parent -eq "Personal") {
     exit
 }
 
+# locate content by parent folder and name
+$FolderNameToExport = "Search Audit Custom"
 $export_id =  ($parent_folder.children | where {$_.ItemType -eq "Folder" -and $_.name -eq $FolderNameToExport}).id
+$export_item_path = get-ContentPath -id $export_id
+$item_by_path = get-ContentByPath -path $export_item_path
 
-
+# only this version icludes children via folder api
+$child_item = get-Folder -id $export_id
+$export_item = run-ContentExportJob -id '0000000000AB8526'
