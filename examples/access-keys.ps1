b49075379680e83a3b188eda9ee9f95f3a49d93a
Rick Jury <rjury@sumologic.com>
2021-10-13 13:25:13 +1300

    access keys map to user

---
+++ examples/access-keys.ps1
@@ -0,0 +1,30 @@
+
+param ([string]$output = "object", $endpoint='au')
+
+$sumo=new-contentsession -endpoint $endpoint
+
+$users = Get-Users
+$keys = Get-AccessKeys   
+
+$keymap = @{}
+
+foreach ($key in $keys) {
+    $current = $key | convertto-json -depth 10 | convertfrom-json -depth 10
+    $current | Add-Member -MemberType NoteProperty -Name 'email' -Value (($users | where { $_.id -eq $current.createdBy }).email)
+    $current | Add-Member -MemberType NoteProperty -Name 'userIsActive' -Value (($users | where { $_.id -eq $current.createdBy }).IsActive)
+    
+    if ($null -eq $current.email ) {
+        $current.email="None"
+        $current.userIsActive=$false
+    }
+
+    $keymap[$current.id] =  $current
+
+}
+
+
+if ($output -eq 'json') {
+    write-output ($keymap | convertto-json -depth 10)
+} elseif ($output -eq 'object') {
+    write-output $keymap
+} 
