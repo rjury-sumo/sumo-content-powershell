d39c9d84c97a2216089107ea311794f86a3180f7
Rick Jury <rjury@sumologic.com>
2021-12-09 16:16:59 +1300

    1.0.9 dashboard export

--- sumo-content-powershell/sumo-content-powershell.psm1
+++ sumo-content-powershell/sumo-content-powershell.psm1
@@ -46,6 +46,8 @@ public class SumoAPISession
 "@ 
 }
 
+$global:scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+
 <#
     .DESCRIPTION
     Create a new sumo session object.
@@ -151,6 +153,9 @@ function getQueryString([hashtable]$form) {
     .PARAMETER returnResponse
     boolean - if set to true will return the whole response object rather than .Content converted to JSON
 
+    .PARAMETER filePath
+    path of a file to execute where required action is -outfile to this path.
+
     .EXAMPLE
     invoke-sumo -path "content/folders/personal"
 
@@ -170,14 +175,15 @@ function invoke-sumo {
         [parameter()]$body,
         [parameter()][string] $v = "v2",
         [parameter()][Hashtable] $headers,
-        [parameter()][bool]$returnResponse = $false
+        [parameter()][bool]$returnResponse = $false #,
+  #      [parameter()][string] $filepath
     )
 
     if ($session -and $session.endpoint) { 
         if ($headers) { } else {
             $headers = @{
                 "content-type" = "application/json";
-                "accept"       = "application/json";
+                "accept"       = "*/*";
                 "isAdminMode"  = $session.isAdminMode
             }
         }
@@ -205,8 +211,11 @@ function invoke-sumo {
             write-verbose "body: `n$body"
             $response = Invoke-WebRequest -Uri $uri -method $method -WebSession $session.WebSession -Headers $headers -Body $body -SkipHttpErrorCheck
     
-        }
-        else {
+  #      }
+ #       elseif ($filepath) {
+ #           $response = Invoke-WebRequest -Uri $uri -method $method -WebSession $session.WebSession -Headers $headers -SkipHttpErrorCheck -OutFile "$filepath"
+
+        } else {
             $response = Invoke-WebRequest -Uri $uri -method $method -WebSession $session.WebSession -Headers $headers -SkipHttpErrorCheck
         }
     }
@@ -217,19 +226,14 @@ function invoke-sumo {
 
     Write-Verbose ($response | Out-String)
 
-    if ($response.statuscode -gt 0 ) {
-        if ($response.statuscode -gt 399) {
-            Write-Error "invoke-sumo $uri returned: $($response.statuscode) StatusDescription $($response.StatusDescription) `n$($response.content)" -ErrorAction Stop
-            return $response
-        }
-
-    }
-    else {
-        Write-Error "invoke-sumo $uri request failed"
-        return @()
+    #if ($response.statuscode -gt 0 -and $returnResponse -ne 1) {
+    if ($response.statuscode -gt 399) {
+        Write-Error "invoke-sumo $uri returned: $($response.statuscode) StatusDescription $($response.StatusDescription) `n$" -ErrorAction Stop
+        return $response
     }
 
     if ($returnResponse) {
+        Write-Verbose "returning whole response object"
         return $response
     }
     else {
@@ -1440,13 +1444,13 @@ function Set-ContentFoldersById {
 function New-Dashboard {
 
     Param(
-         [parameter()][SumoAPISession]$sumo_session = $sumo_session,
-         [parameter(mandatory=$True)]$body
-     )
-     return (invoke-sumo -path "dashboards" -method POST -session $sumo_session -v 'v2' -body $body )
- }
+        [parameter()][SumoAPISession]$sumo_session = $sumo_session,
+        [parameter(mandatory = $True)]$body
+    )
+    return (invoke-sumo -path "dashboards" -method POST -session $sumo_session -v 'v2' -body $body )
+}
  
- <#
+<#
      .DESCRIPTION
      /v2/dashboards/{id},delete
  
@@ -1461,16 +1465,16 @@ function New-Dashboard {
  #>
  
  
- function Remove-DashboardById {
+function Remove-DashboardById {
  
     Param(
-         [parameter()][SumoAPISession]$sumo_session = $sumo_session,
-         [parameter(mandatory=$True)]$id
-     )
-     return (invoke-sumo -path "dashboards/$id" -method DELETE -session $sumo_session -v 'v2')
- }
+        [parameter()][SumoAPISession]$sumo_session = $sumo_session,
+        [parameter(mandatory = $True)]$id
+    )
+    return (invoke-sumo -path "dashboards/$id" -method DELETE -session $sumo_session -v 'v2')
+}
  
- <#
+<#
      .DESCRIPTION
      /v2/dashboards/{id},get
  
@@ -1485,16 +1489,16 @@ function New-Dashboard {
  #>
  
  
- function Get-DashboardById {
+function Get-DashboardById {
  
     Param(
-         [parameter()][SumoAPISession]$sumo_session = $sumo_session,
-         [parameter(mandatory=$True)]$id
-     )
-     return (invoke-sumo -path "dashboards/$id" -method GET -session $sumo_session -v 'v2')
- }
+        [parameter()][SumoAPISession]$sumo_session = $sumo_session,
+        [parameter(mandatory = $True)]$id
+    )
+    return (invoke-sumo -path "dashboards/$id" -method GET -session $sumo_session -v 'v2')
+}
  
- <#
+<#
      .DESCRIPTION
      /v2/dashboards/{id},put
  
@@ -1512,15 +1516,15 @@ function New-Dashboard {
  #>
  
  
- function Set-DashboardById {
+function Set-DashboardById {
  
     Param(
-         [parameter()][SumoAPISession]$sumo_session = $sumo_session,
-         [parameter(mandatory=$True)]$id,
-         [parameter(mandatory=$True)]$body
-     )
-     return (invoke-sumo -path "dashboards/$id" -method PUT -session $sumo_session -v 'v2' -body $body )
- }
+        [parameter()][SumoAPISession]$sumo_session = $sumo_session,
+        [parameter(mandatory = $True)]$id,
+        [parameter(mandatory = $True)]$body
+    )
+    return (invoke-sumo -path "dashboards/$id" -method PUT -session $sumo_session -v 'v2' -body $body )
+}
  
  
 
@@ -1547,14 +1551,14 @@ function New-Dashboard {
 function Get-DashboardContentIdById {
 
     Param(
-         [parameter()][SumoAPISession]$sumo_session = $sumo_session,
-         [parameter(mandatory=$True)]$id
-     )
-     $decimalid = (invoke-sumo -path "dashboard/contentId/$id" -method GET -session $sumo_session -v 'v1alpha')
-     return (convertSumoDecimalContentIdToHexId $decimalid )
- }
+        [parameter()][SumoAPISession]$sumo_session = $sumo_session,
+        [parameter(mandatory = $True)]$id
+    )
+    $decimalid = (invoke-sumo -path "dashboard/contentId/$id" -method GET -session $sumo_session -v 'v1alpha')
+    return (convertSumoDecimalContentIdToHexId $decimalid )
+}
 
- <#
+<#
     .DESCRIPTION
     substitute all strings matching a regular expression in panels with a new string.
 
@@ -1573,42 +1577,267 @@ function Get-DashboardContentIdById {
     .OUTPUTS
     PSCustomObject. A new dashboard object with substitutions.
 #>
- function Edit-DashboardPanelQueries {
+function Edit-DashboardPanelQueries {
 
     Param(
-         [parameter()][SumoAPISession]$sumo_session = $sumo_session,
-         [parameter(mandatory=$True)]$dashboard,
-         [parameter(mandatory=$True)][string]$pattern,
-         [parameter(mandatory=$True)][string]$replacewith
+        [parameter()][SumoAPISession]$sumo_session = $sumo_session,
+        [parameter(mandatory = $True)]$dashboard,
+        [parameter(mandatory = $True)][string]$pattern,
+        [parameter(mandatory = $True)][string]$replacewith
 
-     )
+    )
 
-     if ($dashboard.panels) {
+    if ($dashboard.panels) {
 
         # make a fresh dashboard so we don't trash the origional one in memory.
         $newdash = $dashboard | convertto-json -depth 100 | ConvertFrom-Json -Depth 100
 
-         $p=-1
+        $p = -1
         foreach ($panel in $dashboard.panels) {
-            $p = $p +1
+            $p = $p + 1
             $q = -1
             $changes = 0
             foreach ($query in $panel.queries) {
                 $q = $q + 1
                 $query_instance = $query.queryString
-                        if ($query_instance -match $pattern) {
-                            Write-Verbose "matching panel: $p, query $q replacement: $pattern in $query_instance`n"
-                            $changes = $changes + 1
-                            $newdash.panels[$p].queries[$q].queryString = $query_instance -replace  $pattern,$replacewith
+                if ($query_instance -match $pattern) {
+                    Write-Verbose "matching panel: $p, query $q replacement: $pattern in $query_instance`n"
+                    $changes = $changes + 1
+                    $newdash.panels[$p].queries[$q].queryString = $query_instance -replace $pattern, $replacewith
                 }
             }
         }
         Write-Verbose "made $changes changes in $($p + 1) panels in $($q +1 ) queries found."
         return $newdash 
-     } else {
-         Write-Error "Dashboard is invalid must be a dashboard object with panels attribute."
-     }
- }
+    }
+    else {
+        Write-Error "Dashboard is invalid must be a dashboard object with panels attribute."
+    }
+}
+
+<#
+    .DESCRIPTION
+    /v2/dashboards/reportJobs,post
+
+    .PARAMETER sumo_session
+    Specify a session, defaults to $sumo_session
+
+    .PARAMETER body
+    PSCustomObject body for post
+
+    .OUTPUTS
+    PSCustomObject.
+#>
+
+
+function New-DashboardReportJob {
+
+    Param(
+        [parameter()][SumoAPISession]$sumo_session = $sumo_session,
+        [parameter(mandatory = $True)]$body
+    )
+    return (invoke-sumo -path "dashboards/reportJobs" -method POST -session $sumo_session -v 'v2' -body $body )
+}
+ 
+<#
+     .DESCRIPTION
+     /v2/dashboards/reportJobs/{jobId}/result,get
+ 
+     .PARAMETER sumo_session
+     Specify a session, defaults to $sumo_session
+ 
+     .PARAMETER jobId
+     jobId for get
+
+     .PARAMETER filepath
+     FilePath - path to save output files. defaults to local dir/exports
+ 
+     .PARAMETER writefile
+     boolean defaults true. if false will return object but not write an export file.
+
+     .OUTPUTS
+     PSCustomObject.
+ #>
+ 
+ 
+function Get-DashboardReportJobsResultById {
+ 
+    Param(
+        [parameter()][SumoAPISession]$sumo_session = $sumo_session,
+        [parameter(mandatory = $True)]$jobId,
+        [parameter()][string] $filepath,
+        [parameter()][string] $format = 'Pdf',
+        [parameter()][bool]$writefile = $true 
+    )
+
+    if ($filepath) {
+        # we have a filepath already
+    }
+    else {
+        $filepath = $scriptDir
+        new-item -ItemType Directory "$filepath/exports" -ErrorAction SilentlyContinue | Out-Null
+        $filepath = "$filepath/exports"
+    }
+
+    $filename = "$filepath/$jobid.$($format.tolower())"
+    $result = invoke-sumo -path "dashboards/reportJobs/$jobId/result" -method GET -session $sumo_session -v 'v2' -returnResponse 1 #-filepath $filename
+
+    $job_output = @{
+        'id'          = $jobId;
+        'type'        = $result.Headers["Content-Type"][0];
+        'content'     = $result.Content;
+        'http_status' = $result.StatusCode;
+        'filepath'    = $filename
+    }
+    if ($writefile) {
+        Write-Verbose "writing output file $filename"
+        [System.IO.File]::WriteAllBytes("$filename", $result.Content)
+    }
+
+    return $job_output
+}
+
+<#
+     .DESCRIPTION
+     /v2/dashboards/reportJobs/{jobId}/status,get
+ 
+     .PARAMETER sumo_session
+     Specify a session, defaults to $sumo_session
+ 
+     .PARAMETER jobId
+     jobId for get
+ 
+     .OUTPUTS
+     PSCustomObject.
+ #>
+ 
+ 
+function Get-DashboardReportJobsStatusById {
+ 
+    Param(
+        [parameter()][SumoAPISession]$sumo_session = $sumo_session,
+        [parameter(mandatory = $True)]$jobId
+    )
+    return (invoke-sumo -path "dashboards/reportJobs/$jobId/status" -method GET -session $sumo_session -v 'v2')
+}
+ 
+<#
+    .DESCRIPTION
+    creates an asynchronous job to generate a report from a template, polls for completion and exports a file
+
+    .PARAMETER sumo_session
+    Specify a session, defaults to $sumo_session
+
+    .PARAMETER format
+    pdf or png
+
+    .PARAMETER filepath
+    optional file path defaults to ./export
+
+    .PARAMETER timezone
+    see tz list as per https://api.au.sumologic.com/docs/#operation/generateDashboardReport
+
+    .PARAMETER templateType
+    only DashboardTemplate is implemented at this point
+
+    .PARAMETER id
+    the dashboard template id as per the UI address bar.
+
+    .PARAMETER writefile
+     boolean defaults true. if false will return object but not write an export file.
+    
+    .PARAMETER maxTries
+    number of polling attempts before timeout
+
+    .PARAMETER pollSeconds
+    sleep time between polling attempts
+
+    .OUTPUTS
+    PSCustomObject.
+#>
+
+function Export-DashboardReport {
+    Param(
+        [parameter()][SumoAPISession]$sumo_session = $sumo_session,
+        [parameter()][string] $filepath,
+        [parameter()][string] $exportFormat = 'Pdf',
+        [parameter()][string] $timezone = "America/Los_Angeles",
+        [parameter()][string] $templateType = 'DashboardTemplate',
+        [parameter(mandatory = $True)][string] $id,
+        [parameter()][bool]$writefile = $true ,
+        [parameter()]$maxTries = 60,
+        [parameter()]$pollSeconds = 1
+
+    )
+
+    if ($templateType -eq 'DashboardTemplate') {
+        $exportbody = '{"action":{"actionType":"DirectDownloadReportAction"},"exportFormat":"Pdf","timezone":"America/Los_Angeles","template":{"templateType":"DashboardTemplate"}}' | convertfrom-json -depth 10
+        $exportbody.exportFormat = $exportFormat
+        $exportbody.timezone = $timezone
+        $exportbody.template.templateType = $templateType
+        $exportbody.template | Add-Member -NotePropertyName id -NotePropertyValue $id
+    }
+    
+    $exportJob = New-DashboardReportJob -body $exportbody -sumo_session $sumo_session #-verbose
+    if ($exportjob.id) {
+        $jobid = $exportjob.id
+    }
+    else {
+        write-error "Failed to get a valid dashboard report id"
+        return @()
+    }
+    
+    write-verbose "export: $jobid "
+    $exportstatus = Get-DashboardReportJobsStatusById -jobid $jobid -sumo_session $sumo_session #-verbose
+
+    Write-verbose "status: $($exportstatus | out-string)"
+
+    $tries = 1
+    $last = "none"
+    
+    While ($jobid -and ($maxTries -gt $tries)) {
+        $tries = $tries + 1     
+        Write-Verbose "polling export job $jobid try: $tries of $maxTries"
+        
+        try {
+            $job_state = Get-DashboardReportJobsStatusById -jobid $jobid -sumo_session $sumo_session
+            if ($last -ne $job_state.status) {
+                Write-Verbose "change status: from: $last to $($job_state.status) at $($tries * $pollSeconds) seconds."
+                $last = "$($job_state.status)"
+            }
+            else {
+                Write-Verbose  ($job_state.status)
+            }
+            
+            if ($job_state.status -eq 'Success') {
+                write-verbose "job: $jobid $($job_state.status) after $($tries * $pollSeconds) seconds."
+                break
+            }
+            else {
+                Start-Sleep -Seconds $pollSeconds
+            }
+        }
+        catch {
+            Write-Error "Job status poll error: $jobid `n $($job_state | out-string)"
+            Write-Error $_.ScriptStackTrace
+        }
+    }   
+    Write-Verbose "job poll completed: status: $($job_state.status) jobId: $jobid"
+    if ($job_state.status -ne 'Success') {
+        Write-Error "Job failed or timed out for job: $jobid `n $($job_state | out-string) after $($tries * $pollSeconds) seconds." -ErrorAction Stop; 
+        return 
+    }
+    else {
+        Write-Verbose "export job $jobid is: ($job_state.status) after $($tries * $pollSeconds) seconds)"
+    }
+    
+    $export_result = Get-DashboardReportJobsResultById -jobid $jobid -sumo_session $sumo_session -format $exportFormat
+    $export_result.http_status | Should -Be 200
+    write-verbose "exported: $($export_result.filepath)"
+
+    return $export_result
+    
+}
 
 ######################################################### fieldextrationrules.ps1 functions ##############################################################
 # auto generated by srcgen extractionRules 11/17/2020 2:22:05 PM 
@@ -5454,6 +5683,10 @@ function Get-Users {
 #Export-ModuleMember -Cmdlet Set-DashboardById
 #Export-ModuleMember -Cmdlet Get-DashboardContentIdById
 #Export-ModuleMember -Cmdlet Edit-DashboardPanelQueries
+#Export-ModuleMember -Cmdlet New-DashboardReportJob
+#Export-ModuleMember -Cmdlet Get-DashboardReportJobsResultById
+#Export-ModuleMember -Cmdlet Get-DashboardReportJobsStatusById
+#Export-ModuleMember -Cmdlet Export-DashboardReport
 #Export-ModuleMember -Cmdlet Get-ExtractionRules
 #Export-ModuleMember -Cmdlet New-ExtractionRule
 #Export-ModuleMember -Cmdlet Remove-ExtractionRuleById
